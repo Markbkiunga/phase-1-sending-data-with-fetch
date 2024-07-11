3b62b710f90b7f3fe5850fdae4e681c288acdb5d
Mark Brian <markbkiunga2017@gmail.com>
2024-07-11 09:17:11 +0300

    Completes Lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your code here
+function submitData(userName, userEmail) {
+  let fetchRequest = fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail,
+    }),
+  })
+    .then((response) => response.json())
+    .then((userData) => document.querySelector('body').append(userData.id))
+    .catch((error) => document.querySelector('body').append(error));
+  return fetchRequest;
+}
+submitData('sam', 'sam@sam.com');
